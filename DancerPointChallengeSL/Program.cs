0c2a1e855a5d48dde32d3c701aed66bbca648a8b
tuckerward20 <tuckerward20@gmail.com>
2021-01-22 14:40:38 -0600

    Dance Quiz Upload

---
+++ DancerPointChallengeSL/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DancerPointChallengeSL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = Console.ReadLine();
+            int points1 = Convert.ToInt32(Console.ReadLine());
+            string name2 = Console.ReadLine();
+            int points2 = Convert.ToInt32(Console.ReadLine());
+
+            DancerPoints dancer1 = new DancerPoints(name1, points1);
+            DancerPoints dancer2 = new DancerPoints(name2, points2);
+
+            DancerPoints total = dancer1 + dancer2;
+            Console.WriteLine(total.name);
+            Console.WriteLine(total.points);
+        }
+    }
+
+    class DancerPoints
+    {
+        public string name;
+        public int points;
+        public DancerPoints(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+
+        //overload the + operator
+        public static DancerPoints operator +(DancerPoints a, DancerPoints b)
+        {
+            string n1 = a.name + " & " + b.name;
+            int p2 = a.points + b.points;
+            DancerPoints res = new DancerPoints(n1, p2);
+            return res;
+        }
+    }
+}
